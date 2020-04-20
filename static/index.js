c6c0b024f1d0c89092af9e5f8154514c0c880000
Roman Didukh <roma.didukh@gmail.com>
2020-04-20 18:41:35 +0100

    New UI 3.

--- static/index.js
+++ static/index.js
@@ -1,75 +1,11 @@
 const enabledButtonColor = '#66ff66'
-
-function addTableRow(table, values) {
-    var tr = document.createElement('tr')
-    for (i in values) {
-        value = values[i]
-
-        if (value instanceof HTMLElement) {
-            tr.appendChild(document.createElement('td')).appendChild(value)
-        } else {
-            tr.appendChild(document.createElement('td')).innerHTML = value
-        }
-    }
-    table.appendChild(tr)
-}
-
-function updateStatusTable(status) {
-    var table = document.getElementById('status')
-    table.innerHTML = ''
-    addTableRow(table, ["OK", status.ok])
-    addTableRow(table, ["Quiz", status.quiz_id])
-    addTableRow(table, ["Question", status.question_id])
-    addTableRow(table, ["Registration", status.is_registration])
-    addTableRow(table, ["Language", status.language])
-    addTableRow(table, ["Last updated", new Date()])
-}
-
-function updateTeamsTable(status) {
-    var table = document.getElementById('teams')
-    table.innerHTML = ''
-    for (chat_id in status.teams) {
-        addTableRow(table, [status.teams[chat_id], chat_id])
-    }
-}
-
-function updateAnswersTable(status) {
-    var table = document.getElementById('answers')
-    table.innerHTML = ''
-    team_ids = Object.keys(status.teams);
-    teams_array = team_ids.map(function (id) {
-        return status.teams[id]
-    })
-
-    question_set = status.question_set.sort()
-    addTableRow(table, ['', ''].concat(teams_array))
-    for (const i in question_set) {
-        let question_id = question_set[i];
-        answers = team_ids.map(function (id) {
-            if (question_id in status.answers && id in status.answers[question_id]) {
-                return status.answers[question_id][id]
-            } else {
-                return ''
-            }
-        })
-
-        startButton = document.createElement('button')
-        startButton.innerHTML = 'Start'
-        if (!status.is_registration && !status.question_id) {
-            startButton.style.backgroundColor = enabledButtonColor
-        }
-        startButton.style.border = '1px solid black'
-        startButton.onclick = () => {
-            sendCommand("startQuestion", { "question_id": question_id }).then((response) => {
-                console.log("Question '" + question_id + "' started!")
-            }).catch((error) => {
-                console.warn("Could not start question: " + error)
-            });
-        }
-
-        addTableRow(table, [startButton, question_id].concat(answers))
-    }
-}
+var lastSeenTeamsUpdateId = 0
+var lastSeenAnswersUpdateId = 0
+var currentStatus = { 'update_id': 0 }
+var currentAnswers = null
+const teamsIndex = new Map() // team_id --> Team
+const answersIndex = new Map() // question --> team_id --> Answer
+var currentAnswersTableQuestion = 1
 
 async function sendCommand(command, args) {
     const response = await fetch("/api/" + command, {
@@ -92,34 +28,6 @@ async function sendCommand(command, args) {
         throw data.error;
     }
     return data;
-}
-
-function getStatus() {
-    sendCommand('getStatus', {}).then((response) => {
-        updateStatusTable(response)
-        updateTeamsTable(response)
-        updateAnswersTable(response)
-
-        startRegistrationButton = document.getElementById("start_registration_button")
-        stopRegistrationButton = document.getElementById("stop_registration_button")
-        stopQuestionButton = document.getElementById("stop_question_button")
-
-        if (response.is_registration) {
-            startRegistrationButton.style.backgroundColor = null
-            stopRegistrationButton.style.backgroundColor = enabledButtonColor
-            stopQuestionButton.style.backgroundColor = null
-        } else if (response.question_id) {
-            startRegistrationButton.style.backgroundColor = null
-            stopRegistrationButton.style.backgroundColor = null
-            stopQuestionButton.style.backgroundColor = enabledButtonColor
-        } else {
-            startRegistrationButton.style.backgroundColor = enabledButtonColor
-            stopRegistrationButton.style.backgroundColor = null
-            stopQuestionButton.style.backgroundColor = null
-        }
-    }).catch((error) => {
-        console.log("Could not get status: " + error)
-    });
 }
 
 function startRegistration() {
@@ -146,16 +54,6 @@ function stopQuestion() {
     });
 }
 
-var lastSeenStatusUpdateId = 0
-var lastSeenTeamsUpdateId = 0
-var lastSeenAnswersUpdateId = 0
-var currentStatus = null
-var currentAnswers = null
-const teamsIndex = new Map() // team_id --> Team
-const answersIndex = new Map() // question --> team_id --> Answer
-var currentAnswersTableQuestion = 1
-
-
 function updateTextContent(elementId, newTextContent) {
     const element = document.getElementById(elementId)
     if (element.textContent != newTextContent) {
@@ -163,22 +61,132 @@ function updateTextContent(elementId, newTextContent) {
     }
 }
 
+function updateStatusTable(status) {
+    updateTextContent('status_quiz_cell', status.quiz_id)
+    updateTextContent('status_language_cell', status.language)
+    updateTextContent('status_question_cell', status.question)
+    updateTextContent('status_registration_cell', status.registration.toString())
+    updateTextContent('status_last_changed_cell', status.time)
+}
+
+function createResultsTable(table) {
+    const resultsTableHeaderRow = table.insertRow(0)
+    resultsTableHeaderRow.insertCell(-1)
+    resultsTableHeaderRow.insertCell(-1).textContent = 'Total'
+
+    const resultsStartQuestionRow = table.insertRow(-1)
+    resultsStartQuestionRow.insertCell(-1)
+    resultsStartQuestionRow.insertCell(-1)
+
+    const showAnswersRow = table.insertRow(-1)
+    showAnswersRow.insertCell(-1)
+    showAnswersRow.insertCell(-1)
+
+    for (let question = 1; question <= currentStatus.number_of_questions; question++) {
+        resultsTableHeaderRow.insertCell(-1).textContent = question
+        const startQuestionButton = document.createElement('button')
+        startQuestionButton.textContent = '>'
+        startQuestionButton.onclick = () => {
+            sendCommand("startQuestion", { "question_id": '0' + question.toString() }).then((response) => {
+                console.log("Question '" + question + "' started!")
+            }).catch((error) => {
+                console.warn("Could not start question: " + error)
+            });
+        }
+
+        showAnswersButton = document.createElement('button')
+        showAnswersButton.textContent = 'A'
+        showAnswersButton.onclick = () => {
+            currentAnswersTableQuestion = question
+            updateAnswersTable()
+        }
+
+        resultsStartQuestionRow.insertCell(-1).appendChild(startQuestionButton)
+        showAnswersRow.insertCell(-1).appendChild(showAnswersButton)
+    }
+}
+
+function updateResultsTable() {
+    const table = document.getElementById('results_table')
+
+    if (table.rows.length == 0) {
+        createResultsTable(table)
+    }
+
+    for (const [teamId, team] of teamsIndex) {
+        const rowId = 'results_team_' + team.id + '_row'
+        var row = document.getElementById(rowId)
+        if (!row) {
+            row = table.insertRow(-1)
+            row.id = rowId
+
+            row.insertCell(-1)
+            row.insertCell(-1)
+            for (let q = 1; q <= currentStatus.number_of_questions; q++) {
+                row.insertCell(-1).textContent = '0'
+            }
+        }
+        // TODO: make it a function.
+        if (row.cells[0].textContent != team.name) {
+            row.cells[0].textContent = team.name
+        }
+    }
+}
+
+function updateAnswersTable() {
+    const question = currentAnswersTableQuestion
+    const table = document.getElementById('answers_table')
+
+    var answers = new Map()
+    if (answersIndex.has(question)) {
+        answers = answersIndex.get(question)
+    }
+
+    for (const [teamId, team] of teamsIndex) {
+        const rowId = 'answers_team_' + teamId + '_row'
+        var row = document.getElementById(rowId)
+        if (!row) {
+            row = table.insertRow(-1)
+            row.id = rowId
+            row.insertCell(-1).textContent = team.name
+            row.insertCell(-1) // Answer.
+            row.insertCell(-1) // Correct answer button.
+            row.insertCell(-1) // Wrong answer button.
+        }
+
+        if (!answers.has(teamId)) {
+            // No answer to the question for this team.
+            answerText = ''
+        } else {
+            answerText = answers.get(teamId).answer
+        }
+
+        // TODO: make it a function 
+        if (row.cells[0].textContent != team.name) {
+            row.cells[0].textContent = team.name
+        }
+
+        // TODO: make it a function
+        if (row.cells[1].textContent != answerText) {
+            row.cells[1].textContent = answerText
+        }
+    }
+}
+
 function handleUpdates(updates) {
-    const status = updates.status
-    if (status) {
-        lastSeenStatusUpdateId = status.update_id
-        currentStatus = status
-        updateTextContent('status_quiz_cell', status.quiz_id)
-        updateTextContent('status_language_cell', status.language)
-        updateTextContent('status_question_cell', status.question)
-        updateTextContent('status_registration_cell', status.registration.toString())
-        updateTextContent('status_last_changed_cell', status.time)
+    if (updates.status) {
+        // TODO: what if the quiz id has changed?
+        // TODO: what if the number of questions has changed?
+        currentStatus = updates.status
+        console.log('Status update received.')
+        updateStatusTable(updates.status)
     }
 
     // Update teams index.
     for (const team of updates.teams) {
         lastSeenTeamsUpdateId = Math.max(lastSeenTeamsUpdateId, team.update_id)
         teamsIndex.set(team.id, team)
+        console.log('Team update received. Name: "' + team.name + '". Id: ' + team.id)
     }
 
     // Update answers index.
@@ -187,81 +195,17 @@ function handleUpdates(updates) {
         if (!answersIndex.has(answer.question)) {
             answersIndex.set(answer.question, new Map())
         }
-
         answersIndex.get(answer.question).set(answer.team_id, answer)
+        console.log('Answer update received. Question: ' + answer.question + '. Team Id: ' + answer.team_id + '. Answer: "' + answer.answer + '"')
     }
 
-    // TODO: what if the quiz id has changed?
-    // TODO: what if the number of questions has changed?
-
-    // Create the first row of the results table if it does not exist yet.
-    const resultsTable = document.getElementById('results_table')
-    if (resultsTable.rows.length == 0) {
-        const resultsTableHeaderRow = resultsTable.insertRow(0)
-        resultsTableHeaderRow.insertCell(-1)
-        resultsTableHeaderRow.insertCell(-1).textContent = 'Total'
-        for (let q = 1; q <= currentStatus.number_of_questions; q++) {
-            resultsTableHeaderRow.insertCell(-1).textContent = q
-        }
-    }
-
-    // Update team names in the results table.
-    // TODO: Take only new teams/answers into account.
-    // TODO: iterate through new team updates only?
-    for (const team of updates.teams) {
-        const resultsTeamRowId = 'results_team_' + team.id + '_row'
-        var resultsTeamRow = document.getElementById(resultsTeamRowId)
-        if (!resultsTeamRow) {
-            resultsTeamRow = resultsTable.insertRow(-1)
-            resultsTeamRow.id = resultsTeamRowId
-
-            resultsTeamRow.insertCell(-1)
-            resultsTeamRow.insertCell(-1)
-            for (let q = 1; q <= currentStatus.number_of_questions; q++) {
-                resultsTeamRow.insertCell(-1).textContent = '0'
-            }
-        }
-        // TODO: make it a function.
-        if (resultsTeamRow.cells[0].textContent != team.name) {
-            resultsTeamRow.cells[0].textContent = team.name
-        }
-    }
-
-    // Update team names in the answers table.
-    const answersTable = document.getElementById('answers_table')
-    for (const team of updates.teams) {
-        const answersTeamRowId = 'answers_team_' + team.id + '_row'
-        var answersTeamRow = document.getElementById(answersTeamRowId)
-        if (!answersTeamRow) {
-            answersTeamRow = answersTable.insertRow(-1)
-            answersTeamRow.id = answersTeamRowId
-            answersTeamRow.insertCell(-1).textContent = team.name
-            answersTeamRow.insertCell(-1)
-            answersTeamRow.insertCell(-1)
-            answersTeamRow.insertCell(-1)
-        }
-        if (answersTeamRow.cells[0].textContent != team.name) {
-            answersTeamRow.cells[0].textContent = team.name
-        }
-    }
-
-
-
-    /*
-    <table id='asnwers_table'>
-        <tr id='answers_team_<team_id>_row'>
-            <td>Team Name</td>
-            <td>Answer</td>
-            <td><button>Correct</button></td>
-            <td><button>Incorrect</incorrect></td>
-        </tr>
-    </table>
-    */
+    updateResultsTable()
+    updateAnswersTable()
 }
 
 function getUpdates() {
     sendCommand('getUpdates', {
-        'min_status_update_id': lastSeenStatusUpdateId + 1,
+        'min_status_update_id': currentStatus.update_id + 1,
         'min_teams_update_id': lastSeenTeamsUpdateId + 1,
         'min_answers_update_id': lastSeenAnswersUpdateId + 1,
     }).then((updates) => {
